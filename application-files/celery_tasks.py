c2c13525fc4381d2ed1fc338a44cdfcf0bdd066d
AndreaRylander <andrea_slr@live.se>
2016-10-12 18:27:24 +0000

    got rid of error but still not displaying :(

--- application-files/celery_tasks.py
+++ application-files/celery_tasks.py
@@ -1,15 +1,18 @@
-## the idea based on the toturial https://www.digitalocean.com/community/tutorials/how-to-use-celery-with-rabbitmq-to-queue-tasks-on-an-ubuntu-vps
-
-
 # Import the necessary package to process data in JSON format
-try:
-    import json
-except ImportError:
-    import simplejson as json
-
 from celery import Celery
-from oct2py import octave
-
+from celery.result import AsyncResult
+def make_celery(app):
+	celery=Celery(app.import_name, backend=app.config['CELERY_BACKEND'],
+		broker = app.config['CELERY_BROKER_URL'])
+	celery.conf.update(app.config)
+	TaskBase = celery.Task
+	class ContextTask(TaskBase):
+		abstract = True
+		def __call__(self, *args, **kwargs):
+			with app.app_context():
+				return TaskBase.__call__(self,*args,**kwargs)
+	celery.Task = ContextTask
+	return celery
 
 #octave.run('Table.m')
 
@@ -17,13 +20,7 @@ from oct2py import octave
 
 
 #create a celery application instance that connects to the default RabbitMQ service
-app = Celery('tasks', backend='amqp', broker='amqp://')
+#app = Celery('tasks', backend='amqp', broker='amqp://')
 
 
 #def benchmark(problem,solver):
-
-@app.task
-def benchmark():
-
-	return octave.run('Table.m')
-        
--- application-files/main.py
+++ application-files/main.py
@@ -1,12 +1,11 @@
 #curl -i http://127.0.0.1:5000/benchmark/api/v0.1/tasks
-
-
-from celery_tasks import benchmark
-from flask import Flask,jsonify
+from flask import Flask
+#import oct2py
 from oct2py import octave
+from celery_tasks import make_celery
+
 
 app = Flask(__name__)
-
 #problem = ('problem.m')
 #solver1 =  ('solver1.m')
 #solver2 =  ('solver2.m')
@@ -16,17 +15,32 @@ app = Flask(__name__)
 #get the results via celery worker
 #results = benchmark()
 #results = octave.run('Table.m')
+app = Flask(__name__)
+app.config['CELERY_BROKER_URL']='amqp://guest@localhost//'
+app.config['CELERY_BACKEND']='amqp://'
 
-@app.route('/benchmark/api/v0.1/tasks',methods=['GET'])
+celery = make_celery(app)
+@app.route('/benchmark',methods=['GET'])
 def get_tasks():
     #return jsonify(results1,results2)  #will return the json
     results = benchmark()
-    print 'The times'
-    print results.timeBSeuCallUI()
-
-    print 'The relative errors'
-    print results.relerrBSeuCallUI()
-
+    #print 'The times'
+    octave.run('Table.m')
+    #print results.timeBSeuCallUI()
+    #print 'The relative errors'
+    #print results.relerrBSeuCallU	I()
+    return(("The times: %s, The relative errors: %s" % (octave.timeBSeuCallUI,octave.relerrBSeuCallUI)))
+    #create a celery application instance that connects to the default RabbitMQ service
+#def benchmark(problem,solver)
+@celery.task(name='main.benchmark')
+def benchmark():
+#	oc = oct2py.Oct2Py()
+#	result = oc.run('Table.m')
+	octave.run('Table.m')
+	#res = octave.who()
+	print octave.timeBSeuCallUI()
+	print octave.relerrBSeuCallUI()
+#	return results
 
 if(__name__ == '__main__'):
-    app.run(debug = True)
+    app.run(host='0.0.0.0', debug = True)
