05c8b4cd802bcba9e3359d19ce8156872c7ca8a2
Alessandro Piccolo <alessandro.assisten@gmail.com>
2016-10-17 22:35:19 +0200

    Update celery_tasks.py

--- application-files/celery_tasks.py
+++ application-files/celery_tasks.py
@@ -20,5 +20,6 @@ def benchmark(problem_to_solve):
 	timearray = [item for sublist in time.tolist() for item in sublist]
 	relerrarray = [item for sublist in relerr.tolist() for time in sublist]
 	# Merge three lists into dictionary
+	print({z[0]:list(z[1:]) for z in zip(filepaths,timearray,relerrarray)})
         return {z[0]:list(z[1:]) for z in zip(filepaths,timearray,relerrarray)} 
 
