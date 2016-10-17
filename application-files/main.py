821c190806843a1cf0cecae9e6fd3c11cac71765
Alessandro Piccolo <alessandro.assisten@gmail.com>
2016-10-17 19:36:53 +0200

    Update main.py

--- application-files/main.py
+++ application-files/main.py
@@ -37,7 +37,7 @@ def start_benchmark_task():
     #start_time = time.time()
     # Sends tasks (request) to rabbit
     #for problem_name in problems:
-    problem_to_sovle = 1
+    problem_to_solve = 1
     results = celery.send_task('celery_tasks.benchmark', args = [problem_to_solve]) # results is a list
     allResults.append(results)
     #print (problem_name + " \nThe times:\n %s, \n\n The relative errors:\n %s \n" % (results))
