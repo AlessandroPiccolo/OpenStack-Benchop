3e99242130e7d08cfb78dac886b8126baf845463
Alessandro Piccolo <alessandro.assisten@gmail.com>
2016-10-18 16:24:49 +0200

    Update main.py

--- application-files/main.py
+++ application-files/main.py
@@ -35,20 +35,24 @@ problems = range(1, 2) #range(1, 7) # List, for python 3.x: list(range(1, 7))
 def start_benchmark_task():
     #start_time = time.time()
     # Store the reulsts, name of solver, execution time and relative error
-    all_results = {}
+    #all_results = {}
     # rs = ResultSet([])
     # Sends tasks (request) to rabbit
     #for problem_to_solve in problems:
         #all_results.update(celery.send_task('celery_tasks.benchmark', args = [problem_to_solve]))
         #rs.add(benchmark.delay(problem_to_solve))
+    
     #herp = group(benchmark.delay(problem_to_solve) for problem_to_solve in problems).get()
     #g = group(benchmark.s(1), benchmark.s(2))
-    g = group(benchmark.s(problem_to_solve) for problem_to_solve in problems)
-    g.apply_async()
-    herp = g() 
-    print herp.get()  #we can print it directly 
-    result_maybe = herp.get()
-    print ('results_maybe = %s, type = %s' %(result_maybe, type(result_maybe) ))
+    #g = group(benchmark.s(problem_to_solve) for problem_to_solve in problems)
+    #g.apply_async()
+    #herp = g() 
+    #print herp.get()  #we can print it directly 
+    #result_maybe = herp.get()
+    
+    results = group(benchmark.s(problem_to_solve) for problem_to_solve in problems)().get()
+    print ('results_maybe = %s, type = %s' %(results, type(results) ) )
+    
     #print (problem_name + " \nThe times:\n %s, \n\n The relative errors:\n %s \n" % (results))
     #print("---Execution time %s seconds ---" % (time.time() - start_time))
     return render_template('index.html', result_maybe=result_maybe)
