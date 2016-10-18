28f2a9022825db75a413fbd853686ee9e1a93502
Alessandro Piccolo <alessandro.assisten@gmail.com>
2016-10-18 02:44:29 +0200

    Update celery_tasks.py

--- application-files/celery_tasks.py
+++ application-files/celery_tasks.py
@@ -3,6 +3,7 @@ import os
 from celery import Celery
 from oct2py import octave
 
+# Create cellery worker
 env = os.environ
 CELERY_BROKER_URL = env.get('CELERY_BROKER_URL','amqp://group_11:wearegroup_11@localhost/group_11_vhost'),
 CELERY_RESULT_BACKEND = env.get('CELERY_RESULT_BACKEND','amqp://')
@@ -15,12 +16,10 @@ celery = Celery('tasks',
 # Returns dictionary ['solver': [time, relative_error]]
 @celery.task(name = 'celery_tasks.benchmark')
 def benchmark(problem_to_solve):
-	# Matlab: function [time, relerr, filepaths] = tablee(problem_to_solve)
+	# Matlab: function [time, relerr, filepaths] = tablee(problem_to_solve), problem_to_solve = [1:6]
 	time, relerr, filepaths = octave.tablee(problem_to_solve)
 	# Making time and relerr into normal list and then "flattining" (dont want list in list [[]])
 	timearray = [item for sublist in time.tolist() for item in sublist]
 	relerrarray = [item for sublist in relerr.tolist() for time in sublist]
 	# Merge three lists into dictionary
 	return  {z[0]:list(z[1:]) for z in zip(filepaths,timearray,relerrarray)}
-        #return {'jack': 4098, 'sape': 4139}
-	#return 1
