57fd12af73049fae7ab9615178a7c4af6d0b5f20
Alessandro Piccolo <alessandro.assisten@gmail.com>
2016-10-19 20:07:37 +0200

    Update main.py

--- application-files/main.py
+++ application-files/main.py
@@ -12,6 +12,7 @@ from celery import group
 from celery_tasks import benchmark
 
 import pygal
+from pygal.style import DarkSolarizedStyle
 from pygal.style import Style
 
 # Creates celery worker
@@ -49,10 +50,10 @@ def start_benchmark_task(sig):
 			time_list.append(value[0])
 			relerr_list.append(value[1])
 	
-	purple_color = Style(colors = ('purple') ) 
-	red_color = Style(colors = ('red') ) 
+	custom_style_blue = Style(colors= ('blue') ) 
+	custom_style_red = Style(colors = ('red') ) 
 	
-	line_chart_time = pygal.Bar(style = purple_color)  
+	line_chart_time = pygal.Bar(style=custom_style_blue)  
 	line_chart_time.x_labels = map(str, x_label_string)
 	line_chart_time.add('Time', time_list)
 	line_chart_time.y_title = "Execution time [s]"
@@ -60,7 +61,7 @@ def start_benchmark_task(sig):
 	#line_chart.render()
 	line_chart_time_data = line_chart_time.render_data_uri()
 	
-	line_chart_rerr = pygal.Bar(style = red_color)  
+	line_chart_rerr = pygal.Bar(style=custom_style_red)  
 	line_chart_rerr.x_labels = map(str, x_label_string)
 	line_chart_rerr.add('Relative Error', relerr_list)
 	line_chart_rerr.y_title = "Relative error"
@@ -68,7 +69,7 @@ def start_benchmark_task(sig):
 	#line_chart.render()
 	line_chart_rerr_data = line_chart_rerr.render_data_uri()
 	
-	return render_template("graphing.html", line_chart_time_data = line_chart_time_data, line_chart_rerr_data = line_chart_rerr_data)
+	return render_template("graphing.html", results = results, line_chart_time_data = line_chart_time_data, line_chart_rerr_data = line_chart_rerr_data)
 
 if(__name__ == '__main__'):
 	app.run(host = '0.0.0.0', debug = True)
