bd0fb20d2b28e57daabc5559f0a4c77e0ac05879
Alessandro Piccolo <alessandro.assisten@gmail.com>
2016-10-19 18:41:33 +0200

    Update main.py

--- application-files/main.py
+++ application-files/main.py
@@ -33,31 +33,31 @@ problems = range(1, 4) # List, for python 3.x: list(range(1, 7))
 # in list variable "problem"
 @app.route('/benchmark/<sig>',methods = ['GET'])
 def start_benchmark_task(sig):
-    # Creates all tasks and puts them in queue. get() makes this function wait untill all tasks are completed
-    # All output from the tasks are going to be appended in our results variable (it is a long list)
-    results = group(benchmark.s(problem_to_solve,sig) for problem_to_solve in problems)().get()
+	# Creates all tasks and puts them in queue. get() makes this function wait untill all tasks are completed
+	# All output from the tasks are going to be appended in our results variable (it is a long list)
+	results = group(benchmark.s(problem_to_solve,sig) for problem_to_solve in problems)().get()
     
-    # Create bar chart with pygal
-    line_chart = pygal.Bar()
-    x_label_string = []
-    time_list = []
-    relerr_list = []
+	# Create bar chart with pygal
+	line_chart = pygal.Bar()
+	x_label_string = []
+	time_list = []
+	relerr_list = []
     
-    for i in results:
-        for key, value in i.iteritems():
-            x_label_string.add(key)
-            time_list.add(value[0])
-            relerr_list.add(value[1])
+	for i in results:
+		for key, value in i.iteritems():
+			x_label_string.add(key)
+			time_list.add(value[0])
+			relerr_list.add(value[1])
           
-    line_chart.x_labels = map(x_label_string)
-    line_chart.add('Time', time_list)
-    line_chart.add('Relative Error',  relerr_list)
-    line_chart.y_title = "Relative usage of pronomen in tweets [%]"
-		line_chart.title = "Usage of different pronumens in tweets"
-		#line_chart.render()
-    line_chart_data = line_chart.render_data_uri()
-		
-    return render_template("graphing.html", line_chart_data = line_chart_data)
+	line_chart.x_labels = map(x_label_string)
+	line_chart.add('Time', time_list)
+	line_chart.add('Relative Error',  relerr_list)
+	line_chart.y_title = "Relative usage of pronomen in tweets [%]"
+	line_chart.title = "Usage of different pronumens in tweets"
+	#line_chart.render()
+	line_chart_data = line_chart.render_data_uri()
+	
+	return render_template("graphing.html", line_chart_data = line_chart_data)
 
 if(__name__ == '__main__'):
-    app.run(host = '0.0.0.0', debug = True)
+	app.run(host = '0.0.0.0', debug = True)
